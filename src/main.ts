0ede4fe7b27e5bcf0548f9af3e48ac5f458e8c25
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2024-01-25 05:53:07 +0900

    release: 0.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,36 @@
 import { Plugin } from 'obsidian';
-import { MyPluginSettings, DEFAULT_SETTINGS, SampleSettingTab } from 'settings';
 
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+declare module "obsidian" {
+	interface App {
+		updateFontSize(): void;
+	}
+	interface Vault {
+		getConfig(name: string): any;
+		setConfig(name: string, value: any): void;
+	}
+}
 
+export default class FontSizeAdjuster extends Plugin {
 	async onload() {
-		await this.loadSettings();
-		await this.saveSettings();
-		this.addSettingTab(new SampleSettingTab(this));
-	}
+		this.addCommand({
+			id: 'increment-font-size',
+			name: 'Increment Font Size',
+			callback: () => {
+				const currentSize: number = this.app.vault.getConfig('baseFontSize');
+				this.app.vault.setConfig('baseFontSize', currentSize + 1);
+				this.app.updateFontSize();
+			}
+		});
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		this.addCommand({
+			id: 'decrement-font-size',
+			name: 'Decrement Font Size',
+			callback: () => {
+				const currentSize: number = this.app.vault.getConfig('baseFontSize');
+				this.app.vault.setConfig('baseFontSize', currentSize - 1);
+				this.app.updateFontSize();
+			}
+		});
 	}
 }
